67ea4213273fdf5a4ee625fede0d1c60e8bb1e46
sonots <sonots@gmail.com>
2014-06-16 04:18:21 +0900

    add sample

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+
+gem 'fluent-plugin-stats'
+gem 'fluent-plugin-yohoushi'
---
+++ plugin/out_navigation_timing.rb
@@ -0,0 +1,36 @@
+module Fluent; end
+
+class Fluent::NavigationTimingOutput < Fluent::Output
+  Fluent::Plugin.register_output('navigation_timing', self)
+
+  config_param :tag, :string, :default => nil
+
+  # To support log_level option implemented by Fluentd v0.10.43
+  unless method_defined?(:log)
+    define_method("log") { $log }
+  end
+
+  def configure(conf)
+    super
+  end
+
+  def emit(tag, es, chain)
+    es.each do |time, record|
+      new_record = {}
+      new_record['redirect'] = record['redirectEnd'].to_i - record['redirectStart'].to_i
+      new_record['appCache'] = record['domainLookupStart'].to_i - record['fetchStart'].to_i
+      new_reocrd['dns']      = record['domainLookupEnd'].to_i - record['domainLookupStart'].to_i
+      new_record['tcp']      = record['connectionEnd'].to_i - record['connectionStart'].to_i
+      new_record['request']  = record['responseStart'].to_i - record['requestStart'].to_i
+      new_record['response'] = record['responseEnd'].to_i - record['responseStart'].to_i
+      new_record['dom']      = record['domComplete'].to_i - record['domLoading'].to_i
+      new_record['onLoad']   = record['loadEventEnd'].to_i - record['loadEventStart'].to_i
+      Fluent::Engine.emit(@tag, time, new_record)
+    end
+
+    chain.next
+  rescue => e
+    log.warn e.message
+    log.warn e.backtrace.join(', ')
+  end
+end
